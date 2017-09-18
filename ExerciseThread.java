999829f0e043ed022a4e11d69f07ac49b0904393
LeongShyeLi <shyeli95@gmail.com>
2017-09-18 21:50:18 +0800

    Exercise One Thread

---
+++ ExerciseThread.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package real.time_programming;
+
+/**
+ *
+ * @author Sally Leong
+ */
+public class ExerciseThread extends Thread{
+    
+    public static void main(String[] args){
+        ExerciseThread T1 = new ExerciseThread();
+        T1.start();
+    }
+    
+    @Override
+    public void run(){
+        try{
+            
+        for(int x = 0; x <= 10500; x++){
+            System.out.println(x);
+            sleep(5000);  
+            int y = 0 ;
+            if (x%50 == y && x!=0){
+                System.out.println("hi");
+            }
+        }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+}
